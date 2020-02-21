27fc3b61b3420cc591ae7cede81b6f0afa302eeb
deighvyd@gmail.com <ME#3iteb>
2020-02-21 20:35:35 +1100

    Split OpenGL project into  a Debugger applicaion and an Engine library

--- Debugger/src/pch.h
+++ Debugger/src/pch.h
@@ -2,18 +2,3 @@
 
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#include <windowsx.h>
-
-#include <gl\gl.h>
-#include <gl\glext.h>
-#include <gl\glcorearb.h>
-//#include <gl\glxext.h>
-#include <gl\wglext.h>
-
-#include <assert.h>
-
-#include <math.h>
-#include <string.h>
-
-#include <fstream>
-#include <functional>
